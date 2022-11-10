e2dbb25adac91f335fc3a78a5cd6e0ceb3b0aae8
Elsti97 <elsti.shehati@hotmail.it>
2022-11-10 16:47:59 +0100

    Esercizio svolto

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue(
+  {
+    el: '#root',
+    data:{
+      centrato: 'text-center mt-5 text-primary',
+      titolo: 'Hello World',
+      image: 'https://picsum.photos/1000/500?random=1'
+    }
+  }
+);
